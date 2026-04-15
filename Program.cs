8834bcdbd81fe4c17a4e1ea14814f74677e9b9e9
Sthefany <sthefany.v.silva6@edu.senai.br>
2026-04-15 13:53:20 -0300

    feat: criação inicial do projeto de calculadora

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// namespace organiza nosso codigo
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        
+        static void Main(string[]args)
+        {
+            
+            Console.WriteLine("---Calculadora Senai---");
+
+            Console.Write("Digite o primeiro numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = num1 + num2;
+            Console.WriteLine($"O resultado da soma é: {soma}");
+        }
+    }
+}
